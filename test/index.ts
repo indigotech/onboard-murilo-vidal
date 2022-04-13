9d6431688b4f50bea962bf9aaeda41610196b82d
Murilo V <murilovidal@gmail.com>
2022-04-13 11:38:13 -0300

    supertest setup

--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,24 @@
-import assert from 'assert';
+import { GraphQLServer } from '../src/server';
 
-describe('Example tests', function () {
-  it('should not pass', function () {
-    assert.equal(1, 2);
-  });
-  it('should pass', function () {
-    assert.equal(1, 1);
+const url = `http://localhost:3000/`;
+const request = require('supertest')(url);
+
+before(async () => {
+  const server = new GraphQLServer();
+  await server.startServer();
+});
+
+describe('Graphql helloWorld', function () {
+  it('Returns Hello, world!', (done) => {
+    request
+      .post('graphql')
+      .send({
+        query: '{ helloWorld }',
+      })
+      .end((err: any, res: { body: { data: { helloWorld: string } } }) => {
+        console.log('Printing response:');
+        console.log(res.body.data.helloWorld);
+        done();
+      });
   });
 });
