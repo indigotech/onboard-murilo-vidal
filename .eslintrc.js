7439def65a73c180546e91cbe5e98a4b955c8f28
Murilo V <murilovidal@gmail.com>
2022-04-13 10:23:11 -0300

    test setup and make linter more forgiving with unused vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,5 +46,6 @@ module.exports = {
         },
       },
     ],
+    '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
   },
 };
---
+++ test/index.ts
@@ -0,0 +1,10 @@
+import assert from 'assert';
+
+describe('Example tests', function () {
+  it('should not pass', function () {
+    assert.equal(1, 2);
+  });
+  it('should pass', function () {
+    assert.equal(1, 1);
+  });
+});
