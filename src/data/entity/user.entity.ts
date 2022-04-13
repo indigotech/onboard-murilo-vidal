8591dd7e060d6daeef1c8104adc1feec993f63ed
Murilo V <murilovidal@gmail.com>
2022-04-13 09:58:03 -0300

    removed graphql typing

--- src/data/entity/user.entity.ts
+++ src/data/entity/user.entity.ts
@@ -1,26 +1,19 @@
-import { Field, ID, ObjectType } from 'type-graphql';
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
-@ObjectType()
 export class UserEntity {
-  @Field((type) => ID)
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Field()
   @Column()
   name!: string;
 
-  @Field()
   @Column()
   email!: string;
 
-  @Field()
   @Column()
   dateOfBirth!: string;
 
-  @Field()
   @Column()
   password!: string;
 }
