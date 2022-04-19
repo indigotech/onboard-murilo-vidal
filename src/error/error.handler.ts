52be3aa82ed33828a3936d3d081f3ad4a59ba628
Murilo V <murilovidal@gmail.com>
2022-04-19 17:49:17 -0300

    base error handling

--- src/error/error.handler.ts
+++ src/error/error.handler.ts
@@ -1,25 +1,53 @@
-import { GraphQLError, GraphQLFormattedError } from 'graphql';
+import { formatError, GraphQLError, GraphQLFormattedError } from 'graphql';
 import { ArgumentValidationError } from 'type-graphql';
+import { isBaseError } from './base.error';
 import { DuplicateEmailError } from './duplicate-email.error';
+import { StatusCode } from './error.type';
 import { InternalServerError } from './internal-server.error';
-import { InvalidPasswordError } from './invalid-password.error';
 
 export interface ServerError extends GraphQLFormattedError {
-  code: number;
+  code?: number;
   message: string;
   details?: ArgumentValidationError | string;
 }
+
 export function ErrorHandler(error: GraphQLError): ServerError {
-  if (/(duplicate key)[\s\S]/.test(error.message)) {
+  let data: ServerError = defaultErrorFormatter(error);
+  const { originalError } = error;
+  console.log(error);
+  if (isBaseError(originalError)) {
+    return (data = {
+      code: originalError.code,
+      message: originalError.message,
+    });
+  } else if (originalError instanceof ArgumentValidationError) {
+    const firstMessageParsed = Object.values(
+      originalError.validationErrors?.[0].constraints as { [type: string]: string },
+    )?.[0];
+
+    return (data = {
+      code: StatusCode.BadRequest,
+      message: firstMessageParsed ?? 'Internal Error.',
+    });
+  } else if (/(duplicate key)[\s\S]/.test(error.message)) {
     return new DuplicateEmailError();
-  } else if (error.originalError instanceof ArgumentValidationError) {
-    if (error.originalError.validationErrors[0].property == 'password') {
-      return new InvalidPasswordError();
-    }
-    else {
-      return new InternalServerError()
-    }
   } else {
-    return new InternalServerError()
+    return new InternalServerError();
   }
+}
+
+function defaultErrorFormatter(error: any): ServerError {
+  const data: ServerError = formatError(error);
+
+  if (error?.originalError?.result?.errors?.length === 1) {
+    const originalError = error.originalError.result.errors[0];
+
+    if (originalError.message === error.message) {
+      if (originalError.code) {
+        data.code = originalError.code;
+      }
+    }
+  }
+
+  return data;
 }
--- src/error/invalid-password.error.ts
+++
@@ -1,8 +0,0 @@
-import { BaseError } from './base.error';
-import { ErrorType } from './error.type';
-
-export class InvalidPasswordError extends BaseError {
-  constructor(message = 'Password must be at least 6 characters long and alphanumeric', details?: string) {
-    super(ErrorType.InvalidDataError, message, details);
-  }
-}
